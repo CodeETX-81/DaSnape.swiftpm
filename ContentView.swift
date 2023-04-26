cc9fc90303a177cf30cf52cc767fab3193604dde
Nazar <nkochylo5186@stu.d214.org>
2023-04-26 13:19:09 -0500

    Content

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink {
+                Intro()
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 300, height: 100)
+                    Text("Play")
+                        .foregroundColor(.black)
+                        .font(Font.custom(
+                            "Times New Roman", size: 79))
+                }
+            }
+            
+        }
+        NavigationView {
+            NavigationLink {
+                Achievements()
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 300, height: 100)
+                    Text("Acheievement")
+                        .foregroundColor(.black)
+                        .font(Font.custom(
+                            "Times New Roman", size: 79))
+                }
+            }
+            
+        }
+        NavigationView {
+            NavigationLink {
+                Settings()
+            } label: {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 300, height: 100)
+                    Text("Settings")
+                        .foregroundColor(.black)
+                        .font(Font.custom(
+                            "Times New Roman", size: 79))
+                }
+            }
+            
         }
     }
 }
