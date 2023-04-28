90639b4ed0790a0861ae7b477fcebd73614a11cf
nathan howard <vulpy2688@gmail.com>
2023-04-28 12:42:29 -0500

    achevment page

--- Achievements.swift
+++ Achievements.swift
@@ -8,29 +8,48 @@
 import SwiftUI
 
 struct Achievements: View {
+    @State private var showingAlert = false
     var body: some View {
         ZStack {
             Color.green.ignoresSafeArea() // 1
             VStack {
+                Button("You actually got the game?!") {
+                    showingAlert = true
+                }
+                .font(.title)
+                .frame(maxWidth: .infinity)
+                .background(.red)
+                
+                .alert(isPresented:$showingAlert) {
+                    Alert(
+                        title: Text("Open the game for the first time")
+                        
+                        
+                    )
+                }
+                Divider()
+                Button("So this is a grape") {
+                    showingAlert = true
+                }
+                .font(.title)
+                .frame(maxWidth: .infinity)
+                .background(.red)
+                Divider()
+                
                 Group{
-                    Text("you actually got the game???")
+                    Text("There was a wall there")
                         .font(.title)
                         .frame(maxWidth: .infinity)
                         .background(.red)
                     Divider()
-                  
                 }
                 Group{
-                    Text("So this is a grap")
+                    Text("Where did the board go?")
                         .font(.title)
                         .frame(maxWidth: .infinity)
                         .background(.red)
                     Divider()
                 }
-                Text("")
-                    .font(.title)
-                    .frame(maxWidth: .infinity)
-                    .background(.red)
             }
         }
         .accentColor(Color.black)
