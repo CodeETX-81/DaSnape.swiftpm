cab7ab91d40ba0a91137030522c21fcc75b94fed
Vulpy <vulpy2688@gmail.com>
2023-05-10 12:49:47 -0500

    j

--- Setting.swift
+++ Setting.swift
@@ -10,48 +10,51 @@ import SwiftUI
 struct Settings: View {
     @State var snakecolor:Color = .orange
     @State var background:Color = .green
+    @State var colorChange = true
+    @State var color = true
     var body: some View {
+       
        
         Button("Green"){
             snakecolor = .green
                         }
-        
+
          Button("Blue"){
              snakecolor = .blue
                          }
-        
+
          Button("Red"){
              snakecolor = .red
                          }
-        
+
          Button("Purple"){
              snakecolor = .purple
                          }
-        
+
          Button("White"){
              snakecolor = .white
                          }
-        
+
          Button("Black"){
              snakecolor = .black
                          }
         Button("Green"){
             background = .green
                         }
-        
+
          Button("Blue"){
              background = .blue
                          }
-        
+
          Button("Red"){
              background = .red
                          }
-        
+
          Button("Purple"){
              background = .purple
                          }
-        
-        
-        
+
+
+
     }
 }
