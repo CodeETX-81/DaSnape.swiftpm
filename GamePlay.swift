2ae146ba93d9a742cb22fcb8d58f5361e15bfeb9
Nazar <nkochylo5186@stu.d214.org>
2023-05-16 12:35:59 -0500

    deleyed intro

--- GamePlay.swift
+++ GamePlay.swift
@@ -91,9 +91,9 @@ struct GamePlay: View {
         func updateSnakeSize(){
             if self.snakePositions[0] == self.foodPosition{
                 self.snakePositions.append(self.snakePositions[0])
-                var randomX = Int(Int.random(in: 0...Int(screenWidth))/25) * 25
+                let randomX = Int(Int.random(in: 0...Int(screenWidth))/25) * 25
                 print(randomX)
-                var randomY = Int(Int.random(in: 0...Int(screenHeight))/25) * 25
+                let randomY = Int(Int.random(in: 0...Int(screenHeight))/25) * 25
                 print(randomY)
                 foodPosition = CGPoint(x: randomX, y: randomY)
             }
