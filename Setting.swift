a61f4c1afd60531654ac7584bba6e92bb8bab510
Bohdan <bkondartenko5185@stu.d214.org>
2023-05-16 12:56:11 -0500

    i

--- Setting.swift
+++ Setting.swift
@@ -1,52 +1,34 @@
 import SwiftUI
 
 struct Settings: View {
-    @Binding var SnakeColor:Color 
+    @Binding var SnakeColor:Color
     @State var background:Color = Color.orange
     var body: some View {
-       
-       
-        Button("Green"){
-            SnakeColor = .green
-                        }
-
-         Button("Blue"){
-             SnakeColor = .blue
-                         }
-
-         Button("Red"){
-             SnakeColor = .red
-                         }
-
-         Button("Purple"){
-             SnakeColor = .purple
-                         }
-
-         Button("White"){
-             SnakeColor = .white
-                         }
-
-         Button("Black"){
-             SnakeColor = .black
-                         }
-        Button("Green"){
-            background = .green
-                        }
-
-         Button("Blue"){
-             background = .blue
-                         }
-
-         Button("Red"){
-             background = .red
-                         }
-
-         Button("Purple"){
-             background = .purple
-                         }
-                               
-        
-        
-        
+        VStack{
+            Text("Snake Color")
+            Button("Green"){
+                SnakeColor = .green
+            }
+            
+            Button("Blue"){
+                SnakeColor = .blue
+            }
+            
+            Button("Red"){
+                SnakeColor = .red
+            }
+            
+            Button("Purple"){
+                SnakeColor = .purple
+            }
+            
+            Button("White"){
+                SnakeColor = .white
+            }
+            
+            Button("Black"){
+                SnakeColor = .black
+            }
+        }
     }
 }
