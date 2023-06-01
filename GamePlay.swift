c470d7ded7602333a0827fbcfeb821433b255aa5
Nazar <nkochylo5186@stu.d214.org>
2023-06-01 12:25:04 -0500

    changed opacity

--- GamePlay.swift
+++ GamePlay.swift
@@ -51,7 +51,7 @@ struct GamePlay: View {
                     Image(systemName: "arrowtriangle.up.square.fill")
                 }.scaleEffect(9)
                     .offset(y: -230)
-                    .opacity(0.7)
+                    .opacity(0.8)
                 Button {
                     if SnakeDirection != .up{
                         SnakeDirection = .down}
@@ -60,7 +60,7 @@ struct GamePlay: View {
                     Image(systemName: "arrowtriangle.down.square.fill")
                 }.scaleEffect(9)
                     .offset(y: -80)
-                    .opacity(0.7)
+                    .opacity(0.8)
                 Button {
                     if SnakeDirection != .right{
                         SnakeDirection = .left}
@@ -69,7 +69,7 @@ struct GamePlay: View {
                     Image(systemName: "arrowtriangle.backward.square.fill")
                 }.scaleEffect(9)
                     .offset(x: -150, y: -80)
-                    .opacity(0.7)
+                    .opacity(0.8)
                 Button {
                     if SnakeDirection != .left{
                         SnakeDirection = .right}
@@ -78,7 +78,7 @@ struct GamePlay: View {
                     Image(systemName: "arrowtriangle.forward.square.fill")
                 }.scaleEffect(9)
                     .offset(x: 150, y:-80)
-                    .opacity(0.7)
+                    .opacity(0.8)
             }
         }  .alert(isPresented: $gameOver) {
             Alert(title: Text("You Died"), message: Text(" you hit the wall"), dismissButton: .default(Text("Restart"), action: {
@@ -100,7 +100,7 @@ struct GamePlay: View {
                         .frame(width: 100, height: 39)
                         .background(.orange)
                         .clipShape(RoundedRectangle(cornerRadius: 15))
-                        .opacity(0.7)
+                        .opacity(0.8)
                 }
             }
             ToolbarItem {
@@ -122,7 +122,7 @@ struct GamePlay: View {
                             .frame(width: 100, height: 39)
                             .background(.orange)
                             .clipShape(RoundedRectangle(cornerRadius: 15))
-                            .opacity(0.7)
+                            .opacity(0.8)
 
                     }
                     
@@ -131,7 +131,7 @@ struct GamePlay: View {
                             .frame(width: 100, height: 39)
                             .background(.orange)
                             .clipShape(RoundedRectangle(cornerRadius: 15))
-                            .opacity(0.7)
+                            .opacity(0.8)
                     }
                 }
                 .foregroundColor(.white)
